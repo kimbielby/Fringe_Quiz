b3ed9ed11d09c89638ee2c64b29f4b9e34bfd052
Kim Bielby <kb_f5@yahoo.com>
2016-06-14 21:54:16 +0200

    add questions and answers to html, do scss for background, give answers radio buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/' => 'quizes#index', as: 'questions'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
